52a54813850aa3615c23c9ebff72caca196262b9
mutluhan <mutluhan43@gmail.com>
2019-10-30 23:43:59 +0300

    nothing important here just learning mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -9,18 +10,43 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: { 
-      type: String   
+      type: String,
+      required: true,
+      trim: true,
+    }, 
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw new Error ("Invalid Email")
+            }
+        }
     },
     age: {
-      type: Number
+      type: Number,
+      validate(val) {
+          if(val<0){
+              throw new Error("Age must be a positive number")
+          }
+      }
     }
 })
 
 const me = new User({
     name: 'Mutluhan',
+    email: "myemail@qwerqewr.com             ",
     age: 20
 })
 
+me.save().then(() => {
+    console.log("saved");
+}).catch((error) => {
+    console.log("error occured :",error);
+    
+})
 
 const Task = mongoose.model('Task', {
     description: {
@@ -36,10 +62,10 @@ const testTask = new Task({
     completed: false,
 })
 
-testTask.save().then(() => {
+/*testTask.save().then(() => {
     console.log("saved");
 }).catch((error) =>{
     console.log("error occured" , error);
     
-})
+})*/
 
