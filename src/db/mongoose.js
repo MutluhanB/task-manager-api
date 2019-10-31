1878957109238559221c9cd5879c075a96dd1bbd
mutluhan <mutluhan43@gmail.com>
2019-10-31 13:35:59 +0300

    start creating api

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -8,45 +8,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 })
 
 
-const User = mongoose.model('User', {
-    name: { 
-      type: String,
-      required: true,
-      trim: true,
-    }, 
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if(!validator.isEmail(value)){
-                throw new Error ("Invalid Email")
-            }
-        }
-    },
-    age: {
-      type: Number,
-      validate(val) {
-          if(val<0){
-              throw new Error("Age must be a positive number")
-          }
-      }
-    }
-})
 
-const me = new User({
-    name: 'Mutluhan',
-    email: "myemail@qwerqewr.com             ",
-    age: 20
-})
 
-me.save().then(() => {
-    console.log("saved");
-}).catch((error) => {
-    console.log("error occured :",error);
-    
-})
+
+
 
 const Task = mongoose.model('Task', {
     description: {
@@ -61,11 +26,3 @@ const testTask = new Task({
     description: "test desc",
     completed: false,
 })
-
-/*testTask.save().then(() => {
-    console.log("saved");
-}).catch((error) =>{
-    console.log("error occured" , error);
-    
-})*/
-
