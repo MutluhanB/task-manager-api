1932a7c7ad12ee14a7129033c4fddeaf3af9113e
mutluhan <mutluhan43@gmail.com>
2019-11-01 01:26:38 +0300

    refactored to use async await and created more endpoints

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const userRouters = require('./routers/userRouters')
+const taskRouters = require('./routers/taskRouters')
 require("./db/mongoose")
 
 const User = require("./models/user")
@@ -8,74 +10,12 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(userRouters)
+app.use(taskRouters)
 
-app.get('/users', (req,res) => {
-  User.find({}).then((users) =>{
-    res.send(users)
-  }).catch((e) =>{
-    res.status(500).send()
-  })
-})
 
-app.get('/users/:id' , (req,res)  => {
-  const ID = req.params.id
-
-  User.findById(ID).then((user) =>{
-    if(!user){
-      return res.status(404).send
-    }
-    res.status(200).send(user)
-  }).catch((e) =>{
-    res.status(500).send()
-  })
-
-})
-
-app.post('/users', (req,res)=>{
-  const user = new User(req.body)
-  user.save().then(() => {
-    res.status(201).send(user)
-
-  }).catch((error) =>{
-    console.log("error occured" + error)
-    res.status(400).send(error)
-  })
-
-})
-
-app.post('/tasks', (req,res) =>{
-  const task = new Task(req.body)
-  task.save().then(() => {
-    res.status(201).send(task)
-  }).catch((e) =>{
-    console.log("error occured" + e)
-    res.status(400).send(e)
-  })
-
-})
-
-app.get('/tasks', (req,res) => {
-  Task.find({}).then((tasks) =>{
-    res.status(200).send(tasks)
-  }).catch((e) => {
-    res.status(500).send()
-  })
-})
-
-app.get('/tasks/:id', (req,res) => {
-  const ID = req.params.id
-console.log(ID)
-  Task.findById(ID).then((task) => {
-    if(!task){
-      return res.status(404).send()
-    }
-    res.status(200).send(task)
-  }).catch((e) => {
-    res.status(500).send()
-  })
-
-})
 
 app.listen(port, () => {
   console.log("Server is up on port : " + port)
 })
+
---
+++ src/routers/taskRouters.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const Task = require('../models/task')
+const router = new express.Router()
+
+
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+
+
+})
+
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get('/tasks/:id', async (req, res) => {
+    const ID = req.params.id
+    try {
+        const task = await Task.findById(ID)
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+
+router.delete("/tasks/:id", async (req, res) => {
+    try {
+        const ID = req.params.id
+        const task = await Task.findByIdAndDelete(ID)
+        if (!task) {
+            return res.status(404).send({ error: "task not found for given ID " + ID })
+        }
+        res.send(task)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+
+
+
+router.patch('/tasks/:id', async (req, res) => {
+    console.log("qweqwe");
+
+    const validUpdates = ['description', 'completed']
+    const updates = Object.keys(req.body)
+    const isValidUpdate = updates.every((update) => {
+        return validUpdates.includes(update)
+    })
+
+    if (!isValidUpdate) {
+        return res.status(404).send({ error: "invalid update " })
+    }
+    try {
+        const ID = req.params.id
+
+        const task = await Task.findByIdAndUpdate(ID, req.body, { new: true, runValidators: true })
+
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+module.exports = router
